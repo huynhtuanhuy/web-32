be048cd0a3ad7201d8b4b8df0e3951c7d35a9c20
HuyHT <huynhtuanhuy1996@gmail.com>
2019-10-07 16:06:00 +0700

    Update

--- quyet-de/server.js
+++ quyet-de/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -14,13 +15,39 @@ app.get('/ask', (request, response) => {
     response.sendFile(__dirname + '/views/ask.html');
 });
 
-app.get('/question', (request, response) => {
-    response.sendFile(__dirname + '/views/questionDetail.html');
+// /question/1378
+app.get('/question/:questionIndex', (request, response) => {
+    const fileData = fs.readFileSync("questions.json", "utf-8");
+    const questionList = JSON.parse(fileData);
+    const question = questionList[request.params.questionIndex];
+    
+    if (question) {
+        const questionDetailHTML = fs.readFileSync("views/questionDetail.html", "utf-8");
+        const htmlWithData = questionDetailHTML
+            .replace("question_content", question.content)
+            .replace("total_vote", question.yes + question.no);
+    
+        response.send(htmlWithData);
+    } else {
+        response.send("Câu hỏi không tồn tại!");
+    }
 });
 
 // backend router
 app.post('/add-question', (request, response) => {
-    console.log(request.body.question);
+    const fileData = fs.readFileSync("questions.json", "utf-8");
+    const questionList = JSON.parse(fileData);
+    const questionContent = request.body.question;
+
+    questionList.push({
+        content: questionContent,
+        yes: 0,
+        no: 0,
+    });
+
+    fs.writeFileSync("questions.json", JSON.stringify(questionList));
+
+    response.redirect(`/question/${questionList.length - 1}`);
 });
 
 app.listen(6969, (err) => {
