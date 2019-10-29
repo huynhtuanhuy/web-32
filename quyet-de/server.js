edf6a89aed57385d2c1e78bcad7a3b31346d9ba3
HuyHT <huynhtuanhuy1996@gmail.com>
2019-10-29 22:01:30 +0700

    Update

--- quyet-de/server.js
+++ quyet-de/server.js
@@ -31,22 +31,50 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // frontend router
 app.get('/', (request, response) => {
-    const fileData = fs.readFileSync("questions.json", "utf-8");
-    const questionList = JSON.parse(fileData);
-    const randomIndex = Math.floor(Math.random()*questionList.length);
-    const randomQuestion = questionList[randomIndex];
+    // questionModel.find({})
+    //     .then(questionList => {
+    //         const randomIndex = Math.floor(Math.random()*questionList.length);
+    //         const randomQuestion = questionList[randomIndex];
 
-    if (randomQuestion) {
-        const questionDetailHTML = fs.readFileSync("views/answer.html", "utf-8");
-        const htmlWithData = questionDetailHTML
-            .replace("question_content", randomQuestion.content)
-            .replace("question_index", randomIndex)
-            .replace("question_index_2", randomIndex);
-    
-        response.send(htmlWithData);
-    } else {
-        response.send("Câu hỏi không tồn tại!");
-    }
+    //         if (randomQuestion) {
+    //             const questionDetailHTML = fs.readFileSync("views/answer.html", "utf-8");
+    //             const htmlWithData = questionDetailHTML
+    //                 .replace("question_content", randomQuestion.content)
+    //                 .replace("question_index", randomIndex)
+    //                 .replace("question_index_2", randomIndex);
+            
+    //             response.send(htmlWithData);
+    //         } else {
+    //             response.send("Câu hỏi không tồn tại!");
+    //         }
+    //     }).catch(error => {
+    //         console.log(error);
+    //     });
+
+    questionModel.count({})
+        .then(totalQuestion => {
+            const randomIndex = Math.floor(Math.random()*totalQuestion);
+            questionModel.findOne({}).skip(randomIndex)
+                .then(randomQuestion => {
+                    if (randomQuestion) {
+                        const questionDetailHTML = fs.readFileSync("views/answer.html", "utf-8");
+                        const htmlWithData = questionDetailHTML
+                            .replace("question_content", randomQuestion.content)
+                            .replace("question_index", randomQuestion._id)
+                            .replace("question_index_2", randomQuestion._id);
+                    
+                        response.send(htmlWithData);
+                    } else {
+                        response.send("Câu hỏi không tồn tại!");
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        })
+        .catch(error => {
+            console.log(error);
+        });
 });
 
 app.get('/ask', (request, response) => {
@@ -54,40 +82,47 @@ app.get('/ask', (request, response) => {
 });
 
 // /question/1378
-app.post('/answer/:questionIndex', (request, response) => {
-    const fileData = fs.readFileSync("questions.json", "utf-8");
-    const questionList = JSON.parse(fileData);
-    const question = questionList[request.params.questionIndex];
+app.post('/answer/:questionId', (request, response) => {
+    const questionId = request.params.questionId;
 
-    if (request.body.answer == "yes") {
-        question.yes += 1;
-    } else {
-        question.no += 1;
-    }
+    questionModel.findByIdAndUpdate(
+        questionId,
+        {
+            $inc: { [request.body.answer]: 1 }
+        }
+    ).then(() => {
+        response.redirect(`/question/${questionId}`);
+    }).catch();
 
-    questionList[request.params.questionIndex] = question;
-    fs.writeFileSync("questions.json", JSON.stringify(questionList));
-    // console.log(request.body);
-    response.redirect(`/question/${request.params.questionIndex}`);
+    // question[request.body.answer] += 1
+
+    // questionList[request.params.questionIndex] = question;
+    // fs.writeFileSync("questions.json", JSON.stringify(questionList));
+    // // console.log(request.body);
+    // response.redirect(`/question/${request.params.questionIndex}`);
 });
 
-app.get('/question/:questionIndex', (request, response) => {
-    const fileData = fs.readFileSync("questions.json", "utf-8");
-    const questionList = JSON.parse(fileData);
-    const question = questionList[request.params.questionIndex];
-    
-    if (question) {
-        const questionDetailHTML = fs.readFileSync("views/questionDetail.html", "utf-8");
-        const htmlWithData = questionDetailHTML
-            .replace("question_content", question.content)
-            .replace("total_vote", question.yes + question.no)
-            .replace("vote_yes", question.yes)
-            .replace("vote_no", question.no);
-    
-        response.send(htmlWithData);
-    } else {
-        response.send("Câu hỏi không tồn tại!");
-    }
+app.get('/question/:questionId', (request, response) => {
+    const questionId = request.params.questionId;
+
+    // questionModel.findOne({ _id: questionId })
+    questionModel.findById(questionId) 
+        .then(questionFound => {
+            if (questionFound) {
+                const questionDetailHTML = fs.readFileSync("views/questionDetail.html", "utf-8");
+                const htmlWithData = questionDetailHTML
+                    .replace("question_content", questionFound.content)
+                    .replace("total_vote", questionFound.yes + questionFound.no)
+                    .replace("vote_yes", questionFound.yes)
+                    .replace("vote_no", questionFound.no);
+            
+                response.send(htmlWithData);
+            } else {
+                response.send("Câu hỏi không tồn tại!");
+            }
+        }).catch(error => {
+            console.log(error);
+        });
 });
 
 // backend router
