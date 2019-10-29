63478ca750c582f5b8d0415f6d8ae040f228634b
HuyHT <huynhtuanhuy1996@gmail.com>
2019-10-29 19:11:26 +0700

    Update

--- minihack/app.js
+++ minihack/app.js
@@ -27,10 +27,19 @@ app.get("/game/:gameId", (req, res) => {
         }).join('');
 
         const playerTotalScoreHtml = `
-            <th scope="col">0</th>
-            <th scope="col">0</th>
-            <th scope="col">0</th>
-            <th scope="col">0</th>
+            <th scope="col">Sum of Score (0)</th>
+            <th scope="col" class="player-total-score-0">${game.rows.reduce((total, row) => {
+                return total + row[0];
+            }, 0)}</th>
+            <th scope="col" class="player-total-score-1">${game.rows.reduce((total, row) => {
+                return total + row[1];
+            }, 0)}</th>
+            <th scope="col" class="player-total-score-2">${game.rows.reduce((total, row) => {
+                return total + row[2];
+            }, 0)}</th>
+            <th scope="col" class="player-total-score-3">${game.rows.reduce((total, row) => {
+                return total + row[3];
+            }, 0)}</th>
         `;
 
         const rowHtml = game.rows.map((row, index) => {
@@ -38,20 +47,20 @@ app.get("/game/:gameId", (req, res) => {
                 <tr>
                     <th scope="row">${index + 1}</th>
                     <td>
-                        <input class="form-control" type="number" value="0">
+                        <input data-col="0" data-row="${index}" class="form-control score-input" type="number" value="${row[0]}">
                     </td>
                     <td>
-                        <input class="form-control" type="number" value="0">
+                        <input data-col="1" data-row="${index}" class="form-control score-input" type="number" value="${row[1]}">
                     </td>
                     <td>
-                        <input class="form-control" type="number" value="0">
+                        <input data-col="2" data-row="${index}" class="form-control score-input" type="number" value="${row[2]}">
                     </td>
                     <td>
-                        <input class="form-control" type="number" value="0">
+                        <input data-col="3" data-row="${index}" class="form-control score-input" type="number" value="${row[3]}">
                     </td>
                 </tr>
             `
-        });
+        }).join('');
         
         htmlData = htmlData.replace("{{player_names}}", playerNameHtml);
         htmlData = htmlData.replace("{{player_total_scores}}", playerTotalScoreHtml);
@@ -75,6 +84,23 @@ app.post("/add-new-row", (req, res) => {
 
     fs.writeFileSync("./games.json", JSON.stringify(gameList));
     res.send("Add row success!");
+});
+
+app.post("/update-score", (req, res) => {
+    const gameList = JSON.parse(fs.readFileSync("./games.json", "utf-8"));
+    const gameId = req.body.gameId;
+    const col = req.body.col;
+    const row = req.body.row;
+    const value = req.body.value;
+
+    for (let i = 0; i < gameList.length; i++) {
+        if (gameList[i].id == gameId) {
+            gameList[i].rows[row][col] = Number(value);
+        }
+    };
+
+    fs.writeFileSync("./games.json", JSON.stringify(gameList));
+    res.send("Update score success!");
 });
 
 app.post("/create-game", (req, res) => {
--- minihack/public/main.js
+++ minihack/public/main.js
@@ -5,20 +5,22 @@ document.querySelector(".btn-add-row").addEventListener("click", function() {
     newRow.innerHTML = `
         <th scope="row">${rowLength+1}</th>
         <td>
-            <input class="form-control" type="number" value="0">
+            <input data-col="0" data-row="${rowLength}" class="form-control score-input" type="number" value="0">
         </td>
         <td>
-            <input class="form-control" type="number" value="0">
+            <input data-col="1" data-row="${rowLength}" class="form-control score-input" type="number" value="0">
         </td>
         <td>
-            <input class="form-control" type="number" value="0">
+            <input data-col="2" data-row="${rowLength}" class="form-control score-input" type="number" value="0">
         </td>
         <td>
-            <input class="form-control" type="number" value="0">
+            <input data-col="3" data-row="${rowLength}" class="form-control score-input" type="number" value="0">
         </td>
     `;
 
     document.querySelector('tbody').appendChild(newRow);
+
+    addEvent();
 
     fetch(
         "/add-new-row",
@@ -39,3 +41,48 @@ document.querySelector(".btn-add-row").addEventListener("click", function() {
         console.log(err);
     });
 });
+
+function addEvent() {
+    for (let i = 0; i < document.querySelectorAll(".score-input").length; i++) {
+        const element = document.querySelectorAll(".score-input")[i];
+        element.oninput = function(event) {
+            console.log("input");
+            const row = event.target.dataset.row;
+            const col = event.target.dataset.col;
+            const value = event.target.value;
+    
+            let totalPlayerScore = 0;
+    
+            for (let j = 0; j < document.querySelectorAll(`.score-input[data-col="${col}"]`).length; j++) {
+                const score = document.querySelectorAll(`.score-input[data-col="${col}"]`)[j].value;
+                totalPlayerScore += Number(score);
+            }
+    
+            document.querySelector(`.player-total-score-${col}`).innerText = totalPlayerScore;
+            
+            fetch(
+                "/update-score",
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        gameId: window.location.pathname.split("/")[2],
+                        col: col,
+                        row: row,
+                        value: value,
+                    }),
+                }
+            ).then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    }
+}
+
+addEvent();
