f46849e90c4a6cf56aa8e8974983ac8f785bdd16
HuyHT <huynhtuanhuy1996@gmail.com>
2019-11-05 21:11:47 +0700

    Update

--- hotgirl-backend/app.js
+++ hotgirl-backend/app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session')
 
 const app = express();
 
+app.use(session({
+    secret: 'xjnCh40c4CpAn',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 7*24*60*60*1000 //milisecond
+    }
+}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({}));
+
+app.use((req, res, next) => {
+    console.log(req.sessionID);
+    next();
+});
 
 mongoose.connect(
     'mongodb://localhost/hotgirl-32',
